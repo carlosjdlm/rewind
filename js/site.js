6d4d35e9aba17e2f10fce02a04603ae6665ced56
carlosjdlm <carlosjuanmartinez18@gmail.com>
2022-11-24 15:41:02 -0400

    Rewind
    La app casi terminada, la cual te volteabas palabras

--- js/site.js
+++ js/site.js
@@ -1,4 +1,40 @@
-// Call Hello World
-function helloWorld(){
-    alert("helloWorld")
+//Get the strings from the page
+//Controller function
+function getvalues() {
+    
+    document.getElementById("alert").classList.add("invisible");
+    
+    let userstring = document.getElementById("userstring").value;
+    
+    let revstring = reversestring(userstring);
+    
+    displaystring(reverseString);
+
+}
+
+//Reverse the string
+//Logic function
+function reversestring(userstring) {
+    
+    let revstring = [];
+    
+    //reverse a string using a for loop
+    for (let index = userstring.length - 1 ; index >= 0; index--) {
+        revstring += userstring[index];
+        
+    }
+
+    return revstring
+        
+    
+}
+
+//Display the reversed string to the user
+//View Function 
+function displaystring(params) {
+    
+    //write to the page 
+    document.getElementById("msg").innerHTML = `your string reversed is:` ${revstring};
+    //show the alert box
+    document.getElementById("alert").classList.remove("invisible")
 }
